c10f183838da83a71f9ee7ff4baf3eaa5513019e
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-06-05 22:07:21 +0530

    Time: 0 ms (100%), Space: 44.1 MB (53.99%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l=0;
+        int h=nums.length-1;
+        while(l<h)
+        {
+            int mid=(l+h)/2;
+            if(nums[mid]<nums[mid+1])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                h=mid;
+            }
+            
+        }
+        return l;
+
+    }
+}
