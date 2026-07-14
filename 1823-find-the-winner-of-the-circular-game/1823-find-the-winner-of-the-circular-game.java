bbe4abd8910d2447cb3763b2b1a59a9006ec897e
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-07-14 10:53:05 +0530

    Time: 28 ms (39.43%), Space: 42.5 MB (52.89%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,17 +1,32 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        ArrayList<Integer> l1=new ArrayList<>();
+        // ArrayList<Integer> l1=new ArrayList<>();
+        // for(int i=1;i<=n;i++)
+        // {
+        //     l1.add(i);
+        // }
+        // int i=0;
+        // while(l1.size()>1)
+        // {
+        //     int idx=(i+k-1)%(l1.size());
+        //     l1.remove(idx);
+        //     i=idx;
+        // }
+        // return l1.get(0);
+        // aapporach2
+        Queue<Integer> q=new ArrayDeque<>();
         for(int i=1;i<=n;i++)
         {
-            l1.add(i);
+            q.offer(i);
         }
-        int i=0;
-        while(l1.size()>1)
+        while(q.size()>1)
         {
-            int idx=(i+k-1)%(l1.size());
-            l1.remove(idx);
-            i=idx;
+            for(int i=0;i<k-1;i++)
+            {
+                q.offer(q.poll());
+            }
+            q.poll();
         }
-        return l1.get(0);
+        return q.peek();
     }
 }
