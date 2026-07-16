867d12647771f7a911d25dc1f1f4ac195ce6c463
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-07-16 14:09:57 +0530

    Time: 0 ms (100%), Space: 47.1 MB (35.01%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,23 +15,36 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null)
+        // if(root==null)
+        // {
+        //     return null;
+        // }
+        // if(root.val==val)
+        // {
+        //     return root;
+        // }
+        // else if(root.val>val)
+        // {
+        //     return searchBST(root.left,val);
+        // }
+        // else if(root.val<val)
+        // {
+        //     return searchBST(root.right,val);
+        // }
+        // return root;
+        while(root!=null && root.val!=val)
         {
-            return null;
-        }
-        if(root.val==val)
-        {
-            return root;
-        }
-        else if(root.val>val)
-        {
-            return searchBST(root.left,val);
-        }
-        else if(root.val<val)
-        {
-            return searchBST(root.right,val);
+            if(val<root.val)
+            {
+                root=root.left;
+            }
+            else
+            {
+                root=root.right;
+            }
         }
         return root;
+
         
     }
 }
