06b1ef35ff3cca1042dafdf0e96e3a6a1df40c9b
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-07-17 16:42:47 +0530

    Time: 37 ms (47.68%), Space: 84 MB (27.23%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int c=0;
+        int sum=0;
+        for(int i=0;i<costs.length;i++)
+        {
+            sum+=costs[i];
+            if(sum>coins)
+            {
+                break;
+            }
+            c++;
+        }
+        return c;
+    }
+}
