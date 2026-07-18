dcb69bc01642e85e0b38eba681e6ab4bc9d2dc76
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-07-18 14:21:35 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    int []dp;
+    public int fib(int n) {
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    public int solve(int n,int []dp)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+}
