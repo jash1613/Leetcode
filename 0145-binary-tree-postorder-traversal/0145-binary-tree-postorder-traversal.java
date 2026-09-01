9857bfbf0bcad7f070aca82ca0c98a553142bab3
MANCHALA JASHWANTH SAI-45 <mjashwanth2007@gmail.com>
2026-09-01 22:08:06 +0530

    Time: 0 ms (100%), Space: 43.2 MB (52.4%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,19 +14,16 @@
  * }
  */
 class Solution {
+      List<Integer> l1=new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> l1=new ArrayList<>();
-       return postorder(root,l1);
-    }
-    public List<Integer> postorder(TreeNode root,List l1)
-    {
         if(root==null)
         {
             return l1;
         }
-        postorder(root.left,l1);
-        postorder(root.right,l1);
-        l1.add((root.val));
-        return l1;
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        l1.add(root.val);
+    return l1;
+    
     }
 }
